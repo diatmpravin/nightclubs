0c9ea1be518af463e69b4af2c2b2e8d8e8d41815
Pravin Mishra <diatm.pravin.it.07.27@gmail.com>
2012-03-07 17:05:33 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nightclubs::Application.config.secret_token = '1e530abfd0d5ded21c0499e5b8bd6a7f1ab16c880d17c122af220b0afe36601c39d89dd734bb5e8e8b6d72ec2d8653fac28a7b5bd7f006beaff22f3a37cb5c21'
